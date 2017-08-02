a2b2782c6571668b6feebdb26218fe05e158e5ea
littlearphone@outlook.com <littlearphone@outlook.com>
2017-08-02 18:14:42 +0800

    Refresh component

--- wrapper/src/main/java/com/earphone/wrapper/aspect/ResultWrapAspect.java
+++ wrapper/src/main/java/com/earphone/wrapper/aspect/ResultWrapAspect.java
@@ -1,18 +1,18 @@
 package com.earphone.wrapper.aspect;
 
-import com.earphone.wrapper.annotation.LogPoint;
-import com.earphone.wrapper.wrapper.ResultWrapper.ResultWrapperBuilder;
 import com.earphone.common.constant.ResultType;
 import com.earphone.common.exception.NonCaptureException;
 import com.earphone.common.utils.JSONExtend;
+import com.earphone.wrapper.annotation.LogPoint;
+import com.earphone.wrapper.wrapper.ResultWrapper.ResultWrapperBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
@@ -22,11 +22,15 @@ import java.lang.reflect.Method;
  * @createTime 2015-12-2 下午5:05:52
  */
 @Aspect
+@Component
+@Slf4j
 public class ResultWrapAspect {
-    // 日志对象
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    public ResultWrapAspect() {
+        log.info("####################Initial ResultWrapAspect####################");
+    }
+
     // 切面表达式
-    private final String CUT_EXPRESSION = "@annotation(org.springframework.web.bind.annotation.RequestMapping) and @annotation(com.earphone.wrapper.annotation.LogPoint)";
+    private static final String CUT_EXPRESSION = "@annotation(org.springframework.web.bind.annotation.RequestMapping) and @annotation(com.earphone.wrapper.annotation.LogPoint)";
 
     //会调用两次
     @Around(CUT_EXPRESSION)
@@ -36,7 +40,7 @@ public class ResultWrapAspect {
         try {
             Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
             LogPoint annotation = method.getAnnotation(LogPoint.class);
-            logger.info("Invoke:{}", annotation.value());
+            log.info("Invoke:{}", annotation.value());
             Object result = joinPoint.proceed(joinPoint.getArgs());
             if (annotation.wrapped()) {
                 return new ResultWrapperBuilder().setResult(result).builder();
@@ -45,7 +49,7 @@ public class ResultWrapAspect {
         } catch (NonCaptureException e) {
             return new ResultWrapperBuilder().setType(ResultType.FAILURE).builder();
         } catch (Throwable e) {
-            logger.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             return new ResultWrapperBuilder().setError(e.getMessage()).setType(ResultType.FAILURE).builder();
         }
     }
@@ -53,20 +57,34 @@ public class ResultWrapAspect {
     @AfterReturning(value = CUT_EXPRESSION, returning = "result")
     public void after(JoinPoint joinPoint, Object result) throws Throwable {
         if (notBasicType(result)) {
-            logger.info("Return:{}", JSONExtend.asJSON(result));
+            log.info("Return:{}", JSONExtend.asJSON(result));
         } else {
-            logger.info("Return:{}", result);
+            log.info("Return:{}", result);
         }
     }
 
     private boolean notBasicType(Object result) {
-        if (result instanceof String) return false;
-        if (result instanceof Integer) return false;
-        if (result instanceof Long) return false;
-        if (result instanceof Short) return false;
-        if (result instanceof Double) return false;
-        if (result instanceof Float) return false;
-        if (result instanceof Character) return false;
+        if (result instanceof String) {
+            return false;
+        }
+        if (result instanceof Integer) {
+            return false;
+        }
+        if (result instanceof Long) {
+            return false;
+        }
+        if (result instanceof Short) {
+            return false;
+        }
+        if (result instanceof Double) {
+            return false;
+        }
+        if (result instanceof Float) {
+            return false;
+        }
+        if (result instanceof Character) {
+            return false;
+        }
         return !(result instanceof Boolean);
     }
 }
